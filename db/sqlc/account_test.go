346016e87ffb09ee36be9aac05ca92b5db78c1a6
TYtrack <zzz1362194009@163.com>
2021-12-22 01:20:35 +0800

    over

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-14 18:19:58
- * @LastEditTime: 2021-12-21 23:01:14
+ * @LastEditTime: 2021-12-22 01:20:07
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /goproject/src/go_code/银行项目/db/sqlc/account_test.go
@@ -46,7 +46,7 @@ func TestUpdateAccount(t *testing.T) {
 	fmt.Println("TestUpdateAccount")
 	updateAccountParams := UpdateAccountParams{
 		Balance: 899,
-		ID:      2,
+		ID:      1,
 	}
 	err := testQueries.UpdateAccount(context.Background(), updateAccountParams)
 	require.NoError(t, err)
@@ -56,7 +56,7 @@ func TestUpdate2Account(t *testing.T) {
 	fmt.Println("TestUpdate2Account")
 	updateAccoun2tParams := UpdateAccount2Params{
 		Balance: 444,
-		ID:      3,
+		ID:      1,
 	}
 	account, err := testQueries.UpdateAccount2(context.Background(), updateAccoun2tParams)
 	require.NoError(t, err)
@@ -72,13 +72,13 @@ func TestListAccounts(t *testing.T) {
 		Limit:  5,
 		Offset: 1,
 	}
-	accounts, err := testQueries.ListAccounts(context.Background(), listAccountsParams)
+	_, err := testQueries.ListAccounts(context.Background(), listAccountsParams)
 	require.NoError(t, err)
-	t.Fatal(accounts)
+
 }
 
 func TestDeleteAccount(t *testing.T) {
 	fmt.Println("TestDeleteAccount")
-	err := testQueries.DeleteAccount(context.Background(), 2)
+	err := testQueries.DeleteAccount(context.Background(), 1)
 	require.NoError(t, err)
 }
