e9649f4b45cb257bdf9417a33ad999984cd4e0ec
TYtrack <zzz1362194009@163.com>
2021-12-29 16:40:22 +0800

    迁移2.0

---
+++ db/migration/000002_add_users.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "users" (
+  "username" varchar PRIMARY KEY,
+  "hash_password" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z'
+);
+
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
+
+-- CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
+-- 另一种方法
+ALTER TABLE "accounts" ADD CONSTRAINT "owner_currency_key" UNIQUE("owner","currency");
